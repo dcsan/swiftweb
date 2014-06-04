4c1d6f52a8455963e7df6c3a9594e68e7b3e149f
dcsan <dc@pikkle.com>
2014-06-03 23:33:52 -0700

    add swifyweb WKwebview

--- swiftweb/FirstViewController.swift
+++ swiftweb/FirstViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import WebKit
+
 
 class FirstViewController: UIViewController {
-                            
+    var wk:WKWebView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("did load")
+        var frame = self.view.bounds
+        wk = WKWebView(frame: frame)
+        self.view.addSubview(wk)
+        var url = NSURL(string: "http://www.apple.com")
+        var req = NSURLRequest(URL: url)
+        wk!.loadRequest(req)
+        // Do any additional setup after loading the view, typically from a nib.
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
